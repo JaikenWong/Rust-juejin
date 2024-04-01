ff2cfaefacc044866817e07eb51c8c3cf8a0d1ae
JaikenWong <Jiccence@gmail.com>
2024-04-01 16:09:12 +0800

    字符串示例

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let greeting: &str = "Greetings";
+    let planet: &str = "🪐";
+    let mut sentence = String::new();
+    sentence.push_str(greeting);
+    sentence.push_str(", ");
+    sentence.push_str(planet);
+    println!("final sentence: {}", sentence);
+    println!("{:?}", &sentence[0..5]);
+    //println!("{:?}", &sentence[12..13]);
 }
