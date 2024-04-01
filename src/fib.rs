95c47ec2c951968f05b38245246fec2781d6f42c
JaikenWong <Jiccence@gmail.com>
2024-04-01 16:40:18 +0800

    另一种斐波那契写法

--- src/fib.rs
+++ src/fib.rs
@@ -4,11 +4,23 @@ fn fib(n: u32) -> u32 {
         return 1;
     } else {
         // The recursive case.
-        return fib(n-1) + fib(n-2);
+        return fib(n - 1) + fib(n - 2);
     }
+}
+
+fn fib2(n: u32) -> u32 {
+    if n < 2 {
+        return 1;
+    }
+    let mut a0 = 0;
+    let mut a1 = 1;
+    for _ in 2..=n {
+        (a0, a1) = (a1, a0 + a1);
+    }
+    a1
 }
 
 fn main() {
     let n = 20;
-    println!("fib(n) = {}", fib(n));
+    println!("fib(n) = {}, {}", fib(n), fib2(n));
 }
